23df53ea813fe964b88730ab109bc9d64d991465
Ruslan <ruslanvall@yandex.ru>
2022-07-03 22:26:44 +0300

    separate files for modules

--- part_4/shipyardsAndShips.js
+++ part_4/shipyardsAndShips.js
@@ -1,58 +1,5 @@
-const shipyardAndShipModule = (function () {
-  function Shipyard() {
-    this.ships = [];
-    this.type = '';
-    this.wrongMessage = "Wrong ship type.";
-    this.buildShip = function (ship) {
-      this.ships.push(ship);
-      return ship;
-    };
-    this.repairShip = function (ship) {
-      if (!this.checkType(ship)) {
-        return this.wrongMessage;
-      }
-
-      ship.isRepaired = true;
-      return ship;
-    };
-    this.repaintShip = function(ship, color) {
-      ship.color = color;
-      return ship;
-    };
-    this.swapShip = function(ship, param1, param2) {
-      if (!this.checkType(ship)) {
-        return this.wrongMessage;
-      }
-
-      this.filterShips(ship);
-
-      return this.buildShip(param1, param2);
-    };
-    this.filterShips = (ship) => {
-      this.ships = this.ships.filter((el) => el.id !== ship.id);
-      return this.ships;
-    };
-    this.checkType = function (ship) {
-      return ship.type === this.type;
-    };
-  };
-
-  function Ship () {
-    this.id;
-    this.color = 'white';
-    this.isRepaired = false;
-    this.genId = () => {
-      const min = 100000;
-      const max = 999999;
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
-  };
-
-  return {
-    shipyard: function() {return new Shipyard()},
-    ship: function() {return new Ship()},
-  };
-}());
+const ShipyardModule = require('./ShipyarModule');
+const ShipModule = require('./ShipModule');
 
 const MotorShipyard = function () {
   this.type = 'motor';
@@ -69,8 +16,8 @@ const SailingShipyard = function () {
   };
 };
 
-MotorShipyard.prototype = shipyardAndShipModule.shipyard();
-SailingShipyard.prototype = shipyardAndShipModule.shipyard();
+MotorShipyard.prototype = ShipyardModule.Shipyard.shipyard();
+SailingShipyard.prototype = ShipyardModule.Shipyard.shipyard();
 
 const MotorShip = function (enginePower, caseMaterial) {
   this.id = this.genId();
@@ -86,8 +33,8 @@ const SailingShip = function (numberMasts, totalSailArea) {
   this.totalSailArea = totalSailArea;
 };
 
-MotorShip.prototype = shipyardAndShipModule.ship();
-SailingShip.prototype = shipyardAndShipModule.ship();
+MotorShip.prototype = ShipModule.Ship.ship();
+SailingShip.prototype = ShipModule.Ship.ship();
 
 const motorShipyard = new MotorShipyard();
 const sailingShipyard = new SailingShipyard();
