1463795d16970845853f1d8a454feb21ff7fc8f6
Ruslan <ruslanvall@yandex.ru>
2022-07-15 22:44:37 +0300

    removed Logic class, fixed bugs, changed UI

--- part_7/calculator.js
+++ part_7/calculator.js
@@ -1,28 +1,107 @@
-import { Logic } from './logic.js';
 import { UI } from './ui.js';
 
 export class Calculator {
   constructor() {
     this.ui = new UI();
-    this.logic = new Logic(this.ui);
+    this.currentOperand = null;
+    this.previousOperand = null;
+    this.currentOperation = null;
+    this.baseAction = {
+      '+': () => Number(this.currentOperand) + Number(this.previousOperand),
+      '-': () => Number(this.previousOperand) - Number(this.currentOperand),
+      '*': () => Number(this.previousOperand) * Number(this.currentOperand),
+      '/': () => Number(this.previousOperand) / Number(this.currentOperand),
+      '÷': () => Number(this.previousOperand) / Number(this.currentOperand),
+    };
+    this.baseInputOperation = {
+      'c|C': (_) => {
+        this.currentOperand = null;
+        this.previousOperand = null;
+        this.currentOperation = null;
+        this.ui.setDisplayValue('');
+      },
+      '-|+|*|/|÷': (keyValue) => this.twoOperandActions(keyValue),
+      '=': (_) => {
+        this.ui.setDisplayValue(this.performAction());
+      },
+      'Enter': (_) => {
+        this.ui.setDisplayValue(this.performAction());
+      },
+      'Backspace': (_) => {
+        this.currentOperand = this.currentOperand.slice(0, this.currentOperand.length - 1);
+        this.ui.setDisplayValue(this.currentOperand);
+      },
+      '0|1|2|3|4|5|6|7|8|9': (value) => this.numberOperation(value),
+      'default': () => console.log('Incorrect input'),
+    };
   }
   
+  switchCase = (obj = {}, str = '') =>
+    obj[Object.keys(obj).find(ele => ele.toString().split('|').includes(str.toString())) || 'default'];
+
+  twoOperandActions(keyValue) {
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = null;
+    this.currentOperation = keyValue;
+    this.ui.setDisplayValue('');
+  };
+
+  numberOperation(keyValue) {
+    this.currentOperand = this.currentOperand === null
+      ? keyValue
+      : this.currentOperand + keyValue;
+    this.ui.setDisplayValue(this.currentOperand);
+  };
+
+  performAction() {
+    return this.baseAction[this.currentOperation]();
+  };
+
+  performInputOperation = keyValue => {
+    this.switchCase(this.baseInputOperation, keyValue)(keyValue);
+  };
+
+  hundleButtonInteraction = event => {
+    const buttonValue = event.target.textContent;
+    this.performInputOperation(buttonValue);
+  };
+
+  addAction(action) {
+    this.baseAction = { ...this.baseAction, ...action };
+  };
+
+  addInputOperation(operation) {
+    this.baseInputOperation = { ...this.baseInputOperation, ...operation };
+  };
+
+  setCurrentOperation(value) {
+    this.currentOperation = value;
+  };
+
+  setCurrentOperand(value) {
+    this.currentOperand = value;
+  };
+
+  setPreviousOperand(value) {
+    this.previousOperand = value;
+  };
+
   init() {
     document.addEventListener("keydown", event => {
       const keyValue = event.key;
-      this.logic.performInputOperation(keyValue);
+      this.performInputOperation(keyValue);
     });
 
-    this.ui.buttonsBlock.addEventListener("click", this.logic.hundleButtonInteraction);
+    this.ui.buttonsBlock.addEventListener("click", this.hundleButtonInteraction);
   }
 
   addBtn(value, operation, action) {
     this.ui.addBtn(value);
     if (operation) {
-      this.logic.addInputOperation(operation);
+      this.addInputOperation(operation);
     }
     if (action) {
-      this.logic.addAction(action);
+      this.addAction(action);
     }
   }
 };
--- part_7/main.js
+++ part_7/main.js
@@ -4,17 +4,19 @@ function main() {
   const calculator = new Calculator();
   calculator.init();
 
-  const dotOperation = { ".": (value) => {
-    calculator.logic.setCurrentOperand(calculator.logic.currentOperand + value);
-    calculator.ui.setDisplayValue(calculator.logic.currentOperand);
+  const dotOperation = { '.': (value) => {
+    if (!calculator.currentOperand.includes(value)) {
+      calculator.setCurrentOperand(calculator.currentOperand + value);
+    }
+    calculator.ui.setDisplayValue(calculator.currentOperand);
   }};
 
   const sqrtOperation = { 'sqrt': (value) => {
-    calculator.logic.setCurrentOperation(value);
-    calculator.ui.setDisplayValue(calculator.logic.performAction());
+    calculator.setCurrentOperation(value);
+    calculator.ui.setDisplayValue(calculator.performAction());
   }};
 
-  const sqrtAction = { 'sqrt': () => Math.sqrt(Number(calculator.logic.currentOperand)) };
+  const sqrtAction = { 'sqrt': () => Math.sqrt(Number(calculator.currentOperand)) };
 
   calculator.addBtn('.', dotOperation);
   calculator.addBtn('sqrt', sqrtOperation, sqrtAction);
